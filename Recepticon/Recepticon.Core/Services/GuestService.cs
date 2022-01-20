25c13fad5ef1d9d96021b745f799b2d537472fd7
Paul Olabisi <pauloolabisi@gmail.com>
2022-01-20 12:39:00 +0100

    added additional helpers

---
+++ Recepticon/Recepticon.Core/Services/GuestService.cs
@@ -0,0 +1,11 @@
+﻿using Recepticon.Core.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recepticon.Core.Services
+{
+    public class GuestService : IGuestService
+    {
+    }
+}
