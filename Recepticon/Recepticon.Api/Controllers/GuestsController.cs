8cb25b24b4a9392ecbf53f701de83b47c7b0a54c
Paul Olabisi <pauloolabisi@gmail.com>
2022-01-20 15:57:29 +0100

    completed guest service

--- Recepticon/Recepticon.Api/Controllers/GuestsController.cs
+++ Recepticon/Recepticon.Api/Controllers/GuestsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Recepticon.Core.Services.Interfaces;
+using Recepticon.Domain.Guest;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,40 +16,55 @@ namespace Recepticon.Api.Controllers
     [ApiController]
     public class GuestsController : ControllerBase
     {
-        public GuestsController()
+        // TODO: Create DTO for guest
+        private IGuestService _guestService;
+        public GuestsController(IGuestService guestService)
         {
-
+            _guestService = guestService;
         }
-        // GET: api/<GuestsController>
+
+
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IActionResult> GetAllAsync()
         {
-            return new string[] { "value1", "value2" };
+
+            return Ok(await _guestService.GetAll());
         }
 
-        // GET api/<GuestsController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<IActionResult> GetByIdAsync(int id)
         {
-            return "value";
+            if (id < 0)
+            {
+                return BadRequest();
+            }
+
+            var room = await _guestService.GetById(id);
+
+            if (room == null)
+            {
+                return NotFound();
+            }
+            return Ok(room);
         }
 
-        // POST api/<GuestsController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> CreateAsync([FromBody] Guest room)
         {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            return Ok(await _guestService.Create(room));
         }
 
-        // PUT api/<GuestsController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> UpdateAsync(int id, [FromBody] Guest room)
         {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            return Ok(await _guestService.Update(id, room));
         }
 
-        // DELETE api/<GuestsController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
